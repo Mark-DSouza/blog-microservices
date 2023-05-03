c4c344cfa878722eb1a1766eb60a8bd30ea0bb5f
Mark-DSouza <markdsouza434@gmail.com>
2023-05-03 18:29:39 +0530

    Make client render comments by status

--- client/src/CommentList.tsx
+++ client/src/CommentList.tsx
@@ -3,9 +3,21 @@ import React from 'react';
 import { type Comment } from './types';
 
 const CommentList: React.FC<{ comments: Comment[] }> = ({ comments }) => {
-  const renderedComments = comments.map((comment) => (
-    <li key={comment.commentId}>{comment.content}</li>
-  ));
+  const renderedComments = comments.map((comment) => {
+    let content: string;
+    switch (comment.status) {
+      case 'approved':
+        content = comment.content;
+        break;
+      case 'rejected':
+        content = 'This comment has been rejected';
+        break;
+      case 'pending':
+        content = 'This comment is awaiting moderation';
+        break;
+    }
+    return <li key={comment.commentId}>{content}</li>;
+  });
 
   return <ul>{renderedComments}</ul>;
 };
--- client/src/types.ts
+++ client/src/types.ts
@@ -1,6 +1,7 @@
 export type Comment = {
   commentId: string;
   content: string;
+  status: 'pending' | 'approved' | 'rejected';
 };
 
 export type OnSubmitEvent = React.FormEvent<HTMLFormElement>;
